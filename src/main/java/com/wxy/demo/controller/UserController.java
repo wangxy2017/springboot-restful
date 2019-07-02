3dd46a9b115d56a624229e3ce21ab1240c74b221
wxy <wangxiaoyuan@pand-auto.com>
2019-07-02 17:23:39 +0800

    集成Swagger

--- src/main/java/com/wxy/demo/controller/UserController.java
+++ src/main/java/com/wxy/demo/controller/UserController.java
@@ -4,6 +4,8 @@ import com.wxy.demo.core.CommonResponse;
 import com.wxy.demo.entity.User;
 import com.wxy.demo.service.UserService;
 import com.wxy.demo.service.impl.UserServiceImpl;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +20,7 @@ import java.util.List;
  **/
 @RestController
 @RequestMapping("/user")
+@Api(description = "用户管理")
 public class UserController {
     @Autowired
     private UserService userService;
@@ -27,6 +30,7 @@ public class UserController {
      *
      * @return
      */
+    @ApiOperation(value = "查询用户列表", notes = "查询用户列表")
     @GetMapping(value = "/list")
     public CommonResponse list() {
         List<User> list = userService.queryUserList();
